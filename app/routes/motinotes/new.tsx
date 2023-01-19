bd326f8c68b065cb85ad96aa23f21062cecb4cf1
Bj Jackson <smthbrothrjdev@gmail.com>
2023-01-18 23:14:28 -0600

    add /new route functionallity with validation errors

--- app/routes/motinotes/new.tsx
+++ app/routes/motinotes/new.tsx
@@ -1,17 +1,96 @@
+import { ActionArgs } from '@remix-run/node';
+import { json, redirect } from 'react-router';
+import { db } from '~/utils/db.server';
+import { useActionData } from '@remix-run/react';
+import { badRequest } from '~/utils/request.server';
+
+function validateMotiNoteNote(note: string) {
+  if (note.length < 10) {
+    return 'That quote is too short!';
+  }
+}
+
+function validateMotiNoteName(name: string) {
+  if (name.length < 3) {
+    return 'that name is too short!';
+  }
+}
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData();
+  const note = form.get('note');
+  const name = form.get('name');
+
+  if (typeof note !== 'string' || typeof name !== 'string') {
+    return badRequest({
+      fieldErrors: null,
+      fields: null,
+      formError: 'Form not submitted correctly',
+    });
+  }
+
+  const fields = { note, name };
+  const fieldErrors = {
+    note: validateMotiNoteNote(note),
+    name: validateMotiNoteName(name),
+  };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({
+      fieldErrors,
+      fields,
+      formError: null,
+    });
+  }
+
+  const newNote = await db.motiNote.create({
+    data: fields,
+  });
+  return redirect(`/motinotes/${newNote.id}`);
+};
+
 export default function NewMotinoteRoute() {
+  const actionData = useActionData<typeof action>();
+
   return (
     <div>
       <p>Add your own Motinote</p>
       <form method="post">
         <div>
           <label>
-            Note: <textarea name="note" />
+            Note:{' '}
+            <textarea
+              name="note"
+              defaultValue={actionData?.fields.note}
+              aria-invalid={Boolean(actionData?.fieldErrors.note) || undefined}
+              aria-errormessage={
+                actionData?.fieldErrors?.note ? 'note-error' : undefined
+              }
+            />
           </label>
+          {actionData?.fieldErrors?.note ? (
+            <p className="form-validation-error" role="alert" id="note-error">
+              {actionData.fieldErrors.note}
+            </p>
+          ) : null}
         </div>
         <div>
           <label>
-            name: <input type="text" name="name" />
+            name:{' '}
+            <input
+              type="text"
+              name="name"
+              defaultValue={actionData?.fields.name}
+              aria-invalid={Boolean(actionData?.fieldErrors.name) || undefined}
+              aria-errormessage={
+                actionData?.fieldErrors?.name ? 'name-error' : undefined
+              }
+            />
           </label>
+          {actionData?.fieldErrors?.name ? (
+            <p className="form-validation-error" role="alert" id="name-error">
+              {actionData.fieldErrors.name}
+            </p>
+          ) : null}
         </div>
         <div>
           <button type="submit" className="button">
