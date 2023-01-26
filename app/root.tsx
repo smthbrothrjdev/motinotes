14676477f36f46eaf61a877c680178aaf263d1e4
BJ Jackson <bj.jackson.mil@swf.army.mil>
2023-01-26 16:56:45 -0600

    did the seo thing

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,10 @@ export default function root() {
         <meta charSet="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Author: Action Jackson, Subject: MotiNotes"
+        />
         <title>MotiNote!</title>
         <Links />
       </head>
